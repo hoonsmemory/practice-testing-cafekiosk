425d29626f45487bf10b408fd2d6bc210cb7f349
hoonsmemory <dltjdgnsok1@naver.com>
2024-10-24 13:12:02 +0900

    5-3 요구사항1

--- src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -1,6 +1,5 @@
 package sample.cafekiosk.unit.beverage;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,30 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class AmericanoTest {
 
     @Test
-    @DisplayName("")
-    void getName() throws Exception {
-        // given
+    void getName() {
         Americano americano = new Americano();
 
-        // when
-        String name = americano.getName();
-
-        // then
-        assertThat(name).isEqualTo("아메리카노");
+//        assertEquals(americano.getName(), "아메리카노");
+        assertThat(americano.getName()).isEqualTo("아메리카노");
     }
-
 
     @Test
-    @DisplayName("")
-    void getPrice() throws Exception {
-        // given
+    void getPrice() {
         Americano americano = new Americano();
 
-        // when
-        int price = americano.getPrice();
-
-        // then
-        assertThat(price).isEqualTo(4000);
-
+        assertThat(americano.getPrice()).isEqualTo(4000);
     }
+
 }
